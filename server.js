d7f55300faf5a80a5045333d433a5873a506a12d
Kyle Begovich <begovichkyle@gmail.com>
2018-03-01 14:20:06 -0600

    IM POSTING TO A DB OMG

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+
+var db
+
+MongoClient.connect('mongodb://test_app_user:test_app_pw@ds139242.mlab.com:39242/kyle_tutorial_app', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('kyle_tutorial_app')
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
 console.log('May Node be with you')
-
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
-
-//app.get(path, callback)
-
-/*
-app.get('/', (req, res) => {
-  res.send('Hello, Node!')
-})
-*/
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/test.html')
 })
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body)
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to db')
+    res.redirect('/')
+  })
 })
